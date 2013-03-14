a2cb87f740bd1a900ac17aa6e0179809b5e7e3c6
Farzad Afshari <farzad.afshari@wickedroar.com>
2013-03-14 15:16:02 -0400

    Initial Commit

---
+++ nav.php
@@ -0,0 +1,10 @@
+<nav>
+	<a href="index.html#profile">&gt; Profile
+	</a>
+	<a href="index.html#portfolio">&gt; Portfolio
+	</a>
+	<a href="resume.html">&gt; Resume
+	</a>
+	<a href="index.html#contact">&gt; Contact Me
+	</a>
+</nav>
